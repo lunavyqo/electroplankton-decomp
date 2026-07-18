c5b52a0559d0653ca8964b416fbb8fe3a81a5196
Lunavyqo <60808132+lunavyqo@users.noreply.github.com>
2026-07-18 20:18:32 +0300

    match: arm9 simple accessors and trampolines batch 4c (16/16) (#67)
    Vec3i ldm/stm copies, halfword pack, mla index loads, global table
    getters, and arg-prep tail trampolines. Verified mwccarm 1.2/sp2p3.

---
+++ src/arm9/func_0201df1c.c
@@ -0,0 +1,11 @@
+// addr 0x0201df1c size 0x10
+
+typedef struct {
+    int a;
+    int b;
+    int c;
+} Vec3i;
+
+void func_0201df1c(Vec3i *dst, Vec3i *src) {
+    *dst = *src;
+}
---
+++ src/arm9/func_0203598c.c
@@ -0,0 +1,11 @@
+// addr 0x0203598c size 0x10
+
+typedef struct {
+    int a;
+    int b;
+    int c;
+} Vec3i;
+
+void func_0203598c(Vec3i *dst, Vec3i *src) {
+    *dst = *src;
+}
